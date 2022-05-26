4d827b89d8c8bc3628ee87b903d404c3a0cbc28d
KasunShanaka <kshanaka10@gmail.com>
2022-05-26 09:44:56 +0530

    first commit

---
+++ client/src/pages/Homepage/Homepage.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import style from './Homepage.module.scss'
+
+const Homepage = () => {
+  return (
+    <div className={style.container}>
+        <div className={style.body}>
+          <div className={style.searchBar}>
+            <div className={style.contents}>
+              
+            </div>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Homepage
